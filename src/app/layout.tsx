f30cdd5a1654e77971ff36b5ba0b7ed7c5045692
Michael Louis <michaellouis157@gmail.com>
2024-08-14 07:57:00 -0400

    Updated opengraph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,16 +8,6 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Building a Real-Time Shopping Assistant",
   description: "Turn Live video into instant purchases using object recognition",
-  openGraph: {
-    images: [
-      {
-        url: '/meta_image.png',
-        width: 1200,
-        height: 630,
-        alt: 'Cerebrium Real-Time Shopping Assistant',
-      },
-    ],
-  },
 };
 
 export default function RootLayout({
